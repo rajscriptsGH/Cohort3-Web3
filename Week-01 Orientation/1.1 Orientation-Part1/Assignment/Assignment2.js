1d35ff7a0653f353666d83de9791f8574750c8a5
rajyadav63 <vector36razz@gmail.com>
2025-06-23 06:24:42 +0530

    Assignment2 added

---
+++ Week-01 Orientation/1.1 Orientation-Part1/Assignment/Assignment2.js
@@ -0,0 +1,25 @@
+/*
+Assignment #2
+What if I ask you that the input string should start with 100xdevs ? How would the code change?
+*/
+const crypto = require('crypto')
+
+
+
+
+function findHashWithPrefix(prefix) {
+    let input = 0;
+
+    while (true) {
+        let inputStr = '100xdevs' + input.toString();
+        const hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if (hash.startsWith(prefix)) {
+            return { input: inputStr, hash: hash }
+        }
+        input++;
+    }
+}
+
+const result = findHashWithPrefix('00000')
+console.log(`Input: ${result.input}`);
+console.log(`Hash: ${result.hash}`);
